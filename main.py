5da1201c65cbb74807680381958d19dd6a1f148f
Karolina <karosupel@gmail.com>
2025-05-29 13:41:17 +0200

    write_file z json

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ def read_file(path):
         if ext == 'json':
             return json.load(f)
 
+def write_file(data, path):
+    ext = path.lower().split('.')[-1]
+    with open(path, 'w', encoding='utf-8') as f:
+        if ext == 'json':
+            json.dump(data, f, indent=2, ensure_ascii=False)
+
 def main():
     if len(sys.argv) != 3:
         print("Użycie: program.exe pathToFile1 pathToFile2")
